5529bc4c9f86bd35df266dfc1c7f2d6d84fdd9f6
lfstr <alexander.lofstrand@tcs.com>
2021-10-01 10:34:40 +0200

    copied changed from main cba

--- src/main.js
+++ src/main.js
@@ -6,24 +6,49 @@ var credentials = {}
 
 
 function signUp() {
-    validatePassword();
-    validatePhone();
+    pwdSuccess = validatePassword();
+    phoneSuccess = validatePhone();
+    //success = false
     
-    alert("Success: You are now registered!")
-    alert("Failed: Please enter an email with!")
-
-    //On success
-    document.getElementById("regform").reset();
+    if (pwdSuccess && phoneSuccess) {
+        alert("Success: You are now registered!")
+        let email = document.getElementById("userEmail").value
+        let pwd = document.getElementById("userPassword").value
+        credentials[email] = pwd
+        document.getElementById("regform").reset();
+        //redirect to login page
+    }
 
     return false
 }
 
 function validatePassword() {
+    console.log("Validating pwd")
     //Password 8 char and 1 special
+    let pwd = document.getElementById("userPass").value
+    var format = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/;
+    if (pwd.length < 8 || format.test(pwd) == false) {
+        alert("Please make sure you password is atleast 8 characters long and contains one special character")
+        return false
+    }
+    return true
 }
 
 function validatePhone() {
+    console.log("Validating phone")
     // Approve + - and numbers
+    let phone = document.getElementById("userContactNum").value
+    let format = /^[+]*[(]{0,1}[0-9]{1,3}[)]{0,1}[-\s\./0-9]*$/g
+    if (format.test(phone)) {
+        return true
+    }else {
+        alert("Please format your phone number correctly")
+        return false
+    }
+
+
+
+
 }
 
 function lockOut() {
