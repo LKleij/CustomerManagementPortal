ed643cab38b6ad96fc8c87ed4eafbf8a163ddbaf
BayramFatih1 <91720735+BayramFatih1@users.noreply.github.com>
2021-10-05 09:43:47 +0200

    Update Customer.java

--- US01/JDBCDemo_222Sep2021/JDBCDemo_22Sep2021/src/com/ilp/bean/Customer.java
+++ US01/JDBCDemo_222Sep2021/JDBCDemo_22Sep2021/src/com/ilp/bean/Customer.java
@@ -1,55 +1,69 @@
 package com.ilp.bean;
 
 public class Customer {
+	private String Password;
+	private String Name;
+	private String ContactNumber;
+	private String Address;
+	private String email;
 	
-	private int customerId;
-	private String customerName;
-	private int age;
-	private String city;
-	
-	public Customer(int customerId, String customerName, int age, String city) {
+	public Customer(String Password, String Name, String ContactNumber, String Address, String email) {
 		super();
-		this.customerId = customerId;
-		this.customerName = customerName;
-		this.age = age;
-		this.city = city;
+		this.Password = Password;
+		this.Name = Name;
+		this.ContactNumber = ContactNumber;
+		this.Address = Address;
+		this.email = email;
 	}
-
-	public Customer() {
+	
+	public Customer(){
 		
 	}
-
-	public int getCustomerId() {
-		return customerId;
-	}
-
-	public void setCustomerId(int customerId) {
-		this.customerId = customerId;
-	}
-
-	public String getCustomerName() {
-		return customerName;
-	}
-
-	public void setCustomerName(String customerName) {
-		this.customerName = customerName;
-	}
-
-	public int getAge() {
-		return age;
-	}
-
-	public void setAge(int age) {
-		this.age = age;
-	}
-
-	public String getCity() {
-		return city;
-	}
-
-	public void setCity(String city) {
-		this.city = city;
-	}
 	
 
+	public Customer(int i, String string, int j, String string2) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getPassword() {
+		return Password;
+	}
+
+	public void setPassword(String password) {
+		Password = password;
+	}
+
+	public String getName() {
+		return Name;
+	}
+
+	public void setName(String name) {
+		Name = name;
+	}
+
+	public String getContactNumber() {
+		return ContactNumber;
+	}
+
+	public void setContactNumber(String contactNumber) {
+		ContactNumber = contactNumber;
+	}
+
+	public String getAddress() {
+		return Address;
+	}
+
+	public void setAddress(String address) {
+		Address = address;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	
 }
